09174246766163aba6edd0cebd89ae7df7061251
technolog <viktor@allz.su>
2019-05-31 01:03:28 +0300

    add:smilo:tests

--- test/wallet_address_validator.js
+++ test/wallet_address_validator.js
@@ -414,6 +414,21 @@ describe('WAValidator.validate()', function () {
             valid('VFe9XzzKKhEHeSoPKWhVkrZrPYpZQrQkJF', 'verium');
             valid('VWPG4HB22HbBNgbWoaSAzjApUMJ52Kq4kv', 'VRM');
         });
+
+        it('should return true for correct Smilo addresses', function () {
+            valid('0xE37c0D48d68da5c5b14E5c1a9f1CFE802776D9FF', 'smilo');
+            valid('0x20824d4ff9115cc38ec2103d05fd13b1002d3dd5', 'smilo');
+            valid('0xAff4d6793F584a473348EbA058deb8caad77a288', 'XSM');
+            valid('0xc6d9d2cd449a754c494264e1809c50e34d64562b', 'XSM');
+            valid('0x52908400098527886E0F7030069857D2E4169EE7', 'XSM');
+            valid('0x8617E340B3D01FA5F11F306F4090FD50E238070D', 'XSM');
+            valid('0xde709f2102306220921060314715629080e2fb77', 'XSM');
+            valid('0x27b1fdb04752bbc536007a920d24acb045561c26', 'XSM');
+            valid('0x5aAeb6053F3E94C9b9A09f33669435E7Ef1BeAed', 'XSM');
+            valid('0xfB6916095ca1df60bB79Ce92cE3Ea74c37c5d359', 'XSM');
+            valid('0xdbF03B407c01E7cD3CBea99509d93f8DDDC8C6FB', 'XSM');
+            valid('0xD1220A0cf47c7B9Be7A2E6BA89F429762e7b9aDb', 'XSM');
+        });
     });
 
     describe('invalid results', function () {
@@ -642,6 +657,14 @@ describe('WAValidator.validate()', function () {
 
         it('should return false for incorrect verium addresses', function () {
             commonTests('verium');
+        });
+
+        it('should return false for incorrect smilo eip55 addresses', function () {
+            invalid('6xAff4d6793F584a473348EbA058deb8caad77a288', 'smilo');
+            invalid('0x02fcd51aAbB814FfFe17908fbc888A8975D839A5', 'smilo');
+            invalid('0XD1220A0CF47C7B9BE7A2E6BA89F429762E7B9ADB', 'smilo');
+            invalid('aFf4d6793f584a473348ebA058deb8caad77a2885', 'smilo');
+            invalid('0xff4d6793F584a473', 'smilo');
         });
     });
 });
